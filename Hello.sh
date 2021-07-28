8d2976d04ee0633efd3f49ad6e011f02b7df2c92
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2021-07-28 14:26:02 -0500

    Add files via upload

---
+++ Hello.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+MovieGetter () {
+    echo "Please enter your top 3 movies: "
+    read movie1 movie2 movie3
+    echo "Your top 3 movies are: $movie1 $movie2 $movie3"
+}
+
+MovieGetter
